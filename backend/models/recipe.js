b2668784fb6d04d5f8523f2ab7a007fe77038252
Ammaramd22 <ammaramd22@gmail.com>
2025-10-22 22:05:10 +0530

    Add Recipe, Upload image using multer package

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const recipeSchema = new mongoose.Schema({
+const recipeSchema = new mongoose.Schema(
+  {
     title: { type: String, required: true },
-    ingredients: { type: String, required: true },
+    ingredients: { type: Array, required: true },
     instructions: { type: String, required: true },
     time: { type: String },
-    coverimage: { type: String},
-}, { timestamps: true })
+    coverimage: { type: String },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Recipe", recipeSchema);
--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getRecipes, getRecipe, addRecipe, editRecipe,deleteRecipe } = require("../controller/recipe");
+const { getRecipes, getRecipe, addRecipe, editRecipe,deleteRecipe,upload } = require("../controller/recipe");
 const router = express.Router();
 
 router.get("/",getRecipes) //get all recipes
 router.get("/:id",getRecipe) //get recipe by id
-router.post("/",addRecipe) //add new recipe
+router.post("/",upload.single('file'),addRecipe) //add new recipe
 router.put("/:id",editRecipe) //update recipe
 router.delete("/:id",deleteRecipe) //delete recipe
 
